074928db5dfefc6ab01d3c263a87e25fb7f07423
iponhuer <ireneponce@MacBook-Air-de-Irene.local>
2026-02-04 20:38:45 +0100

    Perímetro rectángulo y cambio PI

--- Perim.R
+++ Perim.R
@@ -2,5 +2,8 @@ PerimetroCuadrado <- function(L){
   return(4*L)
 }
 LongCircunf <- function(L){
-  return(2*pi*L)
+  return(2*3.14159*L)
+}
+PerimetroRectangulo <- function(L,l){
+  return(2*L + 2*l)
 }
